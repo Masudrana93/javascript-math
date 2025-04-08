ffd083107dc68a84d827a654e98026d7618d3bf3
Masud Rana <mr007masudrana@gmail.com>
2025-04-08 13:49:43 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,81 @@
+// Addition(+) two variables to get total value 
+
+    const eggPrice = 10;
+    const onionPrice = 20;
+
+    const total = eggPrice + onionPrice
+
+    console.log(total);
+
+
+    // subtraction(-) two variables to get remaing value 
+
+    const potatoPrice = 15;
+    const tomatoPrice = 20;
+    const result = potatoPrice - tomatoPrice
+
+    console.log(result);
+
+    const milkPrice = 50;
+    const sugarPrice = 40;
+    const result2 = milkPrice - sugarPrice;
+
+    console.log(result2);
+
+    // Multiplication(*) multiply two variables to get result value 
+
+    const dimPrice = 25;
+    const aloPrice = 20;
+
+    const result3 = dimPrice * aloPrice
+
+    console.log(result3);
+
+// Division(/) divide two variables to get result value 
+
+const banaPrice = 40;
+const melonPrice = 20;
+
+console.log(banaPrice / melonPrice);
+
+const fishPrice = 50;
+const meatPrice = 15;
+
+console.log(fishPrice / meatPrice);
+
+
+// modules or remainder operator (%) Vagses varkorar er jonno 
+
+console.log(11 % 7);
+console.log(10 % 3);
+
+// for addition(+) two string variables' values 
+
+const first = "Masud"
+const second = "Rana"
+
+console.log(first + '      ' + second);
+
+
+const sugar = 20;
+const daddy = '20';
+
+console.log(sugar + daddy);
+
+// Variable names caculate with variables value 
+
+const price = 35;
+const inboxPrice = price + 10;
+
+console.log(inboxPrice);
+
+const oldPrice = 50;
+const newPrice = oldPrice + 40;
+const discountPrice = newPrice - 10;
+
+console.log(discountPrice);
+
+
+// practice homework 
+
+1. 
